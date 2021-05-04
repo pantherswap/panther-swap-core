c22cee0d48c2a77ce3fc39a0f0c923bae93e3f5f
panther-swap <admin@pantherswap.com>
2021-05-04 10:58:15 +0800

    Update pancake to panther

--- contracts/PancakeERC20.sol
+++ contracts/PancakeERC20.sol
@@ -1,13 +1,13 @@
 pragma solidity =0.5.16;
 
-import './interfaces/IPancakeERC20.sol';
+import './interfaces/IPantherERC20.sol';
 import './libraries/SafeMath.sol';
 
-contract PancakeERC20 is IPancakeERC20 {
+contract PantherERC20 is IPantherERC20 {
     using SafeMath for uint;
 
-    string public constant name = 'Pancake LPs';
-    string public constant symbol = 'Cake-LP';
+    string public constant name = 'PantherSwap LPs';
+    string public constant symbol = 'PANTHER-LP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
@@ -79,7 +79,7 @@ contract PancakeERC20 is IPancakeERC20 {
     }
 
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
-        require(deadline >= block.timestamp, 'Pancake: EXPIRED');
+        require(deadline >= block.timestamp, 'Panther: EXPIRED');
         bytes32 digest = keccak256(
             abi.encodePacked(
                 '\x19\x01',
@@ -88,7 +88,7 @@ contract PancakeERC20 is IPancakeERC20 {
             )
         );
         address recoveredAddress = ecrecover(digest, v, r, s);
-        require(recoveredAddress != address(0) && recoveredAddress == owner, 'Pancake: INVALID_SIGNATURE');
+        require(recoveredAddress != address(0) && recoveredAddress == owner, 'Panther: INVALID_SIGNATURE');
         _approve(owner, spender, value);
     }
 }
