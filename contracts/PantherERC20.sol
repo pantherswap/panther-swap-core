d88b03491ed1a3d333f503109bd6224003fc8687
Eve <eveswap.finance@gmail.com>
2021-06-19 16:08:38 +0200

    rebranding to LPs to Apple

--- contracts/PantherERC20.sol
+++ contracts/PantherERC20.sol
@@ -6,8 +6,8 @@ import './libraries/SafeMath.sol';
 contract PantherERC20 is IPantherERC20 {
     using SafeMath for uint;
 
-    string public constant name = 'PantherSwap LPs';
-    string public constant symbol = 'PANTHER-LP';
+    string public constant name = 'Apple LPs';
+    string public constant symbol = 'APPLE-LP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
